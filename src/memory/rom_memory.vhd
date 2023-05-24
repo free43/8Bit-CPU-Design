e8113dbf8540ed184d5ab6ce292f849cd8c71025
free43 <63957797+free43@users.noreply.github.com>
2023-05-24 15:20:12 +0200

    Correction in JMP_NC (NSD process) and alu flags get now fetched after every alg. and logical command

--- src/memory/rom_memory.vhd
+++ src/memory/rom_memory.vhd
@@ -15,16 +15,11 @@ architecture bh of rom_memory is
     signal enable : std_logic := '0';
     constant my_rom : rom := (      0 => LDA_IMM,
                                     1 => x"AA",
-                                    2 => LDA_DIR,
-                                    3 => x"02",
-                                    4 => LDB_DIR,
-											   5 => x"04",
-											   6 => LDB_IMM,
-											   7 => x"05",
-											   8 => STA_DIR,
-												9 => x"80",
-												10 => STB_DIR,
-												11 => x"81",
+                                    2 => LDB_IMM,
+                                    3 => x"00",
+                                    4 => ADD_AB,
+                                    5 => JMP_NN,
+                                    6 => x"00",
                                     others => x"00" );
 begin
     enable <= '1' when address >= x"00" and address <= x"7F" else '0';
